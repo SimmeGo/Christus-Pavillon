0753a7d728795c313e6d0a8660abbb710ba18f14
SimmeGo <84033699+SimmeGo@users.noreply.github.com>
2021-05-18 12:34:21 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,2 +1,33 @@
 (()=>{"use strict";console.log("Script started successfully"),WA.openCoWebSite("https://workadventu.re")})();
 //# sourceMappingURL=script.js.map
+=======
+WA.sendChatMessage('Hello world', 'Mr Robot');
+
+class Popup {
+    /**
+     * Closes the popup
+     */
+    close() {};
+}
+
+//openPopup("WasserText", 'Hallo!', [label: "OK", className: "normal", callback: (popup) => popup.close()]);
+
+/**let helloWorldPopup;
+
+// Open the popup when we enter a given zone
+helloWorldPopup = WA.onEnterZone('WasserZone', () => {
+    WA.openPopup("WasserText", 'Hello world!', [{
+        label: "Schliessen",
+        className: "primary",
+        callback: (popup) => {
+            // Close the popup when the "Close" button is pressed.
+            popup.close();
+        }
+    }]);
+});
+
+// Close the popup when we leave the zone.
+WA.onLeaveZone('WasserZone', () => {
+    helloWorldPopup.close();
+});
+*/
